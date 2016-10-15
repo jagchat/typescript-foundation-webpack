60e7a05629422178f9ca1954e3ecc6f060a0e75e
Der-Alex <alexander.thimm@googlemail.com>
2016-10-15 10:12:17 +0200

    removed unnecessary stuff from webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
-var webpack = require("webpack");
-var path = require("path");
-
 module.exports = {
     entry: './app/app.ts',
     output: {
         filename: './app/bundle.js'
     },
     resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+        extensions: ['', '.ts', '.js']
     },
     module: {
         loaders: [
